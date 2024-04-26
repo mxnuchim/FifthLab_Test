1c2a7050feb08701446d78e5861a0e83998204e4
mxnuchim <manuchimoliver779@gmail.com>
2024-04-26 19:31:20 +0100

    fix: animated loader

--- src/components/LoadingView.tsx
+++ src/components/LoadingView.tsx
@@ -8,12 +8,10 @@ const LoadingView = () => {
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray bg-opacity-50">
       <div className="w-24 h-24">
         <Lottie
-          options={{
-            loop: true,
-            autoplay: true,
-            rendererSettings: {
-              preserveAspectRatio: "xMidYMid slice",
-            },
+          loop
+          autoplay
+          rendererSettings={{
+            preserveAspectRatio: "xMidYMid slice",
           }}
           animationData={loading}
         />
